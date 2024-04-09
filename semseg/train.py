f0b8e210a82ceb118a5a2e3aaf3bad58392a4122
woohyuun <jwh2204@gmail.com>
2024-04-10 02:15:01 +0900

    [Feature] Update baseline code -- implement semseg on CityScape datasets

--- semseg/train.py
+++ semseg/train.py
@@ -1,21 +1,34 @@
 import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+
 import argparse
+import random
+import numpy as np
 from tqdm import tqdm
-from datasets.ai4space import AI4SpaceDataset
-from models import unet
-from utils import metrics
+import matplotlib.pyplot as plt
+
+from datasets import *
+import models
+
 import torch
 import torch.nn as nn
+import albumentations as A
+
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Parameter setting for training segmentation model')
-    parser.add_argument('--root_dir', type=str, help='root directory', required=True)
-    parser.add_argument('--num_classes', type=int, default=10, help='number of classes')
-    parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
-    parser.add_argument('--batch_size', type=int, default=2, help='batch size')
+    parser.add_argument('--model_name', type=str, default='UNet', help='model name')
+    parser.add_argument('--root_dir', type=str, default='D:/study/segmentation/datasets/data', help='root directory')
+    parser.add_argument('--out_dir', type=str, default='D:/study/segmentation/outputs', help='output directory')
+    parser.add_argument('--num_classes', type=int, default=19, help='number of classes')
+    parser.add_argument('--epochs', type=int, default=500, help='number of epochs')
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size')
     parser.add_argument('--img_size', type=int, default=1024, help='image size')
-    parser.add_argument('--num_workers', type=int, default=4, help='number of workers')
+    parser.add_argument('--num_workers', type=int, default=1, help='number of workers')
+    parser.add_argument('--seed', type=int, default=123, help='random seed')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
     parser.add_argument('--weight_decay', type=float, default=0.0001, help='weight decay')
 
@@ -24,103 +37,125 @@ def get_arguments():
     return args
 
 
+def train(model, epoch, device, train_dataloader, optimizer, criterion):
+    model.train()
+
+    train_loss = 0.
+    for data, target in tqdm(train_dataloader, desc='Epoch {} Train'.format(epoch)):
+        data, target = data.to(device), target.to(device)
+
+        # feed forward
+        output = model(data)
+
+        # get loss
+        loss = criterion(output, target)
+        train_loss += loss.item()
+
+        # back propagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(train_dataloader)
+
+    return train_loss
+
+
+def valid(model, epoch, device, valid_dataloader, criterion):
+    model.eval()
+
+    valid_loss = 0.
+    with torch.no_grad():
+        for data, target in tqdm(valid_dataloader, desc='Epoch {} Validation'.format(epoch)):
+            data, target = data.to(device), target.to(device)
+
+            # get predicted
+            output = model(data)
+
+            # get loss
+            loss = criterion(output, target)
+            valid_loss += loss.item()
+
+    valid_loss /= len(valid_dataloader)
+
+    return valid_loss
+
+
 def main():
     # get arguments
     args = get_arguments()
 
-    # get device information
-    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # confirm device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print('device type -- {}'.format(device))
 
-    # define dataset
-    train_dataset = AI4SpaceDataset(data_dir=args.root_dir, split='train')
-    val_dataset = AI4SpaceDataset(data_dir=args.root_dir, split='val')
+    # fix seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.num_workers,
-                                               shuffle=True, drop_last=True)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=args.num_workers,
-                                             shuffle=True, drop_last=True)
+    # generate dataset
+    train_aug = A.Compose([
+        A.RandomCrop(args.img_size, args.img_size),
+        A.OneOf([
+          A.GaussNoise(),
+          A.HorizontalFlip(p=0.5),
+          A.RandomRotate90(p=0.5),
+          A.VerticalFlip(p=0.5)
+        ], p=0.5)
+    ])
 
-    # define model
-    model = unet.UNet(in_channels=3, num_classes=args.num_classes).to(DEVICE)
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    val_aug = A.Compose([
+        A.RandomCrop(args.img_size, args.img_size)
+    ])
+
+    train_dataset = CityScapeDataset(data_dir=args.root_dir, split='train', transform=train_aug)
+    valid_dataset = CityScapeDataset(data_dir=args.root_dir, split='val', transform=val_aug)
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=2*args.num_workers,
+                                                   shuffle=True)
+    valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, num_workers=2*args.num_workers,
+                                                   shuffle=True)
+
+    # set model
+    model = models.__dict__[args.model_name](in_channels=3, num_classes=args.num_classes).to(device)
     criterion = nn.CrossEntropyLoss(ignore_index=255)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=200)
 
-    # train model
-    train_losses, val_losses = [], []
-    train_ious, val_ious = [], []
-    train_accs, val_accs = [], []
-    best_iou = 0
+    # train
+    train_losses = []
+    valid_losses = []
+    min_loss = 9999.
     for epoch in range(args.epochs):
-        # do training
-        epoch_trloss, epoch_triou, epoch_tracc = 0, 0, 0
-        for train_img, train_msk in tqdm(train_loader):
-            model.train()
-            # set device
-            train_img, train_msk = train_img.to(DEVICE), train_msk.to(DEVICE)
-
-            # predict
-            train_out = model(train_img)
-            train_loss = criterion(train_out, train_msk)
-
-            # evaluate
-            train_iou = metrics.estimate_miou(pred_mask=train_out, gt_mask=train_msk, num_classes=args.num_classes)
-            train_acc = metrics.estimate_accuracy(pred_mask=train_out, gt_mask=train_msk)
-
-            # update loss, iou, accuracy
-            epoch_trloss += train_loss.item()
-            epoch_triou += train_iou
-            epoch_tracc += train_acc
-
-            # update model weight
-            optimizer.zero_grad()
-            train_loss.backward()
-            optimizer.step()
-
-        # do validation
-        epoch_valloss, epoch_valiou, epoch_valacc = 0, 0, 0
-        for val_img, val_msk in tqdm(val_loader):
-            with torch.no_grad():
-                model.eval()
-
-                # set device
-                val_img, val_msk = val_img.to(DEVICE), val_msk.to(DEVICE)
-
-                # predict
-                val_out = model(val_img)
-                val_loss = criterion(val_out, val_msk)
-
-                # evaluate
-                val_iou = metrics.estimate_miou(pred_mask=val_out, gt_mask=val_msk, num_classes=args.num_classes)
-                val_acc = metrics.estimate_accuracy(pred_mask=val_out, gt_mask=val_msk)
-
-                # update loss, iou, accuracy
-                epoch_valloss += val_loss.item()
-                epoch_valiou += val_iou
-                epoch_valacc += val_acc
-
-        # update history
-        train_losses.append(epoch_trloss/len(train_loader))
-        val_losses.append(epoch_valloss/len(val_loader))
-        train_ious.append(epoch_triou/len(train_loader))
-        val_ious.append(epoch_valiou/len(val_loader))
-        train_accs.append(epoch_tracc/len(train_loader))
-        val_accs.append(epoch_valacc/len(val_loader))
+        train_loss = train(model, epoch, device, train_dataloader, optimizer, criterion)
+        valid_loss = valid(model, epoch, device, valid_dataloader, criterion)
+        train_losses.append(train_loss)
+        valid_losses.append(valid_loss)
 
         print('epoch: {}/{} -- '.format(epoch+1, args.epochs),
-              'train loss: {:.4f}'.format(epoch_trloss/len(train_loader)),
-              'val loss: {:.4f}'.format(epoch_valloss/len(val_loader)),
-              'train iou: {:.4f}'.format(epoch_triou/len(train_loader)),
-              'val iou: {:.4f}'.format(epoch_valiou/len(val_loader)),
-              'train acc: {:.4f}'.format(epoch_tracc/len(train_loader)),
-              'val acc: {:.4f}'.format(epoch_valacc/len(val_loader)))
+              'train loss: {:.4f}'.format(train_loss),
+              'val loss: {:.4f}'.format(valid_loss)
+              )
 
-        check_iou = epoch_valiou/len(val_loader)
-        if best_iou < check_iou:
-            # update best iou value
-            best_iou = check_iou
+        scheduler.step()
 
-            # save model
-            torch.save(model.state_dict(), os.path.join(args.root_dir, 'model.pth'))
+        if valid_loss < min_loss:
+            min_loss = valid_loss
+            torch.save(model.state_dict(), os.path.join(args.out_dir, args.model_name.lower() + '_best.pth'))
+
+    # save loss changes to image file
+    plt.figure(figsize=(7.5, 5))
+    plt.plot(range(1, len(train_losses) + 1), train_losses, label='train')
+    plt.plot(range(1, len(valid_losses) + 1), valid_losses, label='valid')
+    plt.title('Train-Valid Loss', fontsize=20)
+    plt.xlabel('Epochs', fontsize=15)
+    plt.ylabel('Loss', fontsize=15)
+    plt.xticks(range(1, len(train_losses) + 1), fontsize=12.5)
+    plt.yticks(fontsize=12.5)
+    plt.legend(fontsize=17.5)
+    plt.savefig(os.path.join(args.out_dir, args.model_name.lower() + '_loss.png'))
 
 
 if __name__ == '__main__':
