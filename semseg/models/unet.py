88211b78a55cae33f5848233c67de8cb1b57ba84
whjeon94 <jwh2204@gmail.com>
2024-04-24 14:32:50 +0900

    [Feature] Generate unet & unet++ & unet3+ architecture python file

--- semseg/models/unet.py
+++ semseg/models/unet.py
@@ -1,67 +1,126 @@
 import torch
 import torch.nn as nn
-import torchvision.transforms.functional as TF
 
 
-# set UNet configuration
+__all__ = ['Unet', 'unet']
+
+
+# set Unet configuration
 cfgs = [64, 128, 256, 512, 1024]
 
 
-class UNet(nn.Module):
-    def __init__(self, in_channels, num_classes):
-        super(UNet, self).__init__()
-        # set contracting path
-        self.dims = [in_channels] + cfgs
-        self.encoding = nn.ModuleList(
-            [self.unet_parts(in_dim, out_dim) for in_dim, out_dim in zip(self.dims[:-1], self.dims[1:])]
+class UnetBlock(nn.Module):
+    def __init__(self, in_dims, out_dims):
+        super(UnetBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_dims, out_dims, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=out_dims),
+            nn.ReLU(inplace=True),
         )
-        self.down = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # set expansive path
-        self.up = nn.ModuleList(
-            [nn.ConvTranspose2d(in_dim, out_dim, kernel_size=2, stride=2)
-             for in_dim, out_dim in zip(self.dims[::-1][:-2], self.dims[::-1][1:-1])]
-        )
-        self.decoding = nn.ModuleList(
-            [self.unet_parts(in_dim, out_dim) for in_dim, out_dim in zip(self.dims[::-1][:-2], self.dims[::-1][1:-1])]
-        )
+    def forward(self, x):
+        x = self.conv(x)
+
+        return x
+
+
+class Unet(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(Unet, self).__init__()
+        self.dims = [in_channels] + cfgs
+
+        # contracting path
+        self.enc1_1 = UnetBlock(in_channels, cfgs[0])
+        self.enc1_2 = UnetBlock(cfgs[0], cfgs[0])
+        self.enc2_1 = UnetBlock(cfgs[0], cfgs[1])
+        self.enc2_2 = UnetBlock(cfgs[1], cfgs[1])
+        self.enc3_1 = UnetBlock(cfgs[1], cfgs[2])
+        self.enc3_2 = UnetBlock(cfgs[2], cfgs[2])
+        self.enc4_1 = UnetBlock(cfgs[2], cfgs[3])
+        self.enc4_2 = UnetBlock(cfgs[3], cfgs[3])
+
+        # bridge
+        self.bridge_1 = UnetBlock(cfgs[3], cfgs[4])
+        self.bridge_2 = UnetBlock(cfgs[4], cfgs[4])
+
+        # expansive path
+        self.up_4 = nn.ConvTranspose2d(cfgs[4], cfgs[3], kernel_size=2, stride=2)
+        self.dec4_1 = UnetBlock(cfgs[4], cfgs[3])
+        self.dec4_2 = UnetBlock(cfgs[3], cfgs[3])
+
+        self.up_3 = nn.ConvTranspose2d(cfgs[3], cfgs[2], kernel_size=2, stride=2)
+        self.dec3_1 = UnetBlock(cfgs[3], cfgs[2])
+        self.dec3_2 = UnetBlock(cfgs[2], cfgs[2])
+
+        self.up_2 = nn.ConvTranspose2d(cfgs[2], cfgs[1], kernel_size=2, stride=2)
+        self.dec2_1 = UnetBlock(cfgs[2], cfgs[1])
+        self.dec2_2 = UnetBlock(cfgs[1], cfgs[1])
+
+        self.up_1 = nn.ConvTranspose2d(cfgs[1], cfgs[0], kernel_size=2, stride=2)
+        self.dec1_1 = UnetBlock(cfgs[1], cfgs[0])
+        self.dec1_2 = UnetBlock(cfgs[0], cfgs[0])
+
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # set classifier
         self.classifier = nn.Conv2d(in_channels=cfgs[0], out_channels=num_classes,
                                     kernel_size=1, stride=1, padding=0, bias=False)
 
-    def unet_parts(self, in_channels, out_channels):
-        conv = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.ReLU(inplace=True)
-        )
-
-        return conv
-
     def forward(self, x):
-        # do encoding
-        down_layers = []
-        for enc_layer in self.encoding:
-            x = enc_layer(x)
-            if enc_layer is not self.encoding[-1]:
-                down_layers.append(x)
-                x = self.down(x)
+        # encoding
+        x_enc1_1 = self.enc1_1(x)
+        x_enc1_2 = self.enc1_2(x_enc1_1)
+        x_enc1_out = self.maxpool(x_enc1_2)
 
-        down_layers = down_layers[::-1]
+        x_enc2_1 = self.enc2_1(x_enc1_out)
+        x_enc2_2 = self.enc2_2(x_enc2_1)
+        x_enc2_out = self.maxpool(x_enc2_2)
 
-        # do decoding
-        for up_layer, dec_layer, concat_layer in zip(self.up, self.decoding, down_layers):
-            x = up_layer(x)
-            if x.shape != concat_layer.shape:
-                x = TF.resize(x, concat_layer.shape[2:])
+        x_enc3_1 = self.enc3_1(x_enc2_out)
+        x_enc3_2 = self.enc3_2(x_enc3_1)
+        x_enc3_out = self.maxpool(x_enc3_2)
 
-            x_concat = torch.cat((concat_layer, x), dim=1)
-            x = dec_layer(x_concat)
+        x_enc4_1 = self.enc4_1(x_enc3_out)
+        x_enc4_2 = self.enc4_2(x_enc4_1)
+        x_enc4_out = self.maxpool(x_enc4_2)
 
-        # do classifier
-        x = self.classifier(x)
+        # bridge
+        x_bridge_1 = self.bridge_1(x_enc4_out)
+        x_bridge_2 = self.bridge_2(x_bridge_1)
 
-        return x
+        # decoding
+        x_dec4_in = torch.cat([self.up_4(x_bridge_2), x_enc4_2], dim=1)
+        x_dec4_1 = self.dec4_1(x_dec4_in)
+        x_dec4_2 = self.dec4_2(x_dec4_1)
+
+        x_dec3_in = torch.cat([self.up_3(x_dec4_2), x_enc3_2], dim=1)
+        x_dec3_1 = self.dec3_1(x_dec3_in)
+        x_dec3_2 = self.dec3_2(x_dec3_1)
+
+        x_dec2_in = torch.cat([self.up_2(x_dec3_2), x_enc2_2], dim=1)
+        x_dec2_1 = self.dec2_1(x_dec2_in)
+        x_dec2_2 = self.dec2_2(x_dec2_1)
+
+        x_dec1_in = torch.cat([self.up_1(x_dec2_2), x_enc1_2], dim=1)
+        x_dec1_1 = self.dec1_1(x_dec1_in)
+        x_dec1_2 = self.dec1_2(x_dec1_1)
+
+        out = self.classifier(x_dec1_2)
+
+        return out
+
+
+def unet(**kwargs):
+    model = Unet(**kwargs)
+
+    return model
+
+
+if __name__ == '__main__':
+    img_size = 512
+    model = unet(in_channels=3, num_classes=1000)
+
+    input = torch.randn(1, 3, img_size, img_size)
+    output = model(input)
+
+    print(output.shape)
